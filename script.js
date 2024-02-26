730af206b6c2d24b912eede81ab71d3c6b459bc6
KaiBatley <kaisbat@gmail.com>
2024-02-26 17:29:42 -0600

    changed path of images

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const images = [];
 for (let i = 0; i <= 50; i++) {
     let num = 465 + i;
     const imageNumber = num.toString(); 
-    const imagePath = `/photos/1708_E_Main_St_Little_Chute_WI/IMG_0${imageNumber}.JPG`;
+    const imagePath = `IMG_0${imageNumber}.JPG`;
     images.push(imagePath);
 }
 
