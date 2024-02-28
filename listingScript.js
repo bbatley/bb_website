b3ac782fadfd06e7618173ff499611e34a2fb760
KaiBatley <kaisbat@gmail.com>
2024-02-28 01:06:24 -0600

    listing card selecter working

--- listingScript.js
+++ listingScript.js
@@ -6,13 +6,30 @@ showAllButton.addEventListener('click', () => {
     propertyCards.forEach(card => card.classList.add('show'));
 });
 
+const bedroomDropdown = document.getElementById('bedroom_dropdown');
+const availDropdown = document.getElementById('avail_dropdown');
+
 showSomeButton.addEventListener('click', () => {
     propertyCards.forEach((card, index) => {
         // Example: Show only cards with even indexes
-        if (index % 2 === 0) { 
-            card.classList.add('show');
-        } else {
+        if ((card.getAttribute('avail') === 'yes' && availDropdown.selectedIndex == 1) ||
+                (card.getAttribute('avail') === 'no' && availDropdown.selectedIndex == 0) ||
+                (card.getAttribute('bedroom') === 'one' && bedroomDropdown.selectedIndex == 2) ||
+                (card.getAttribute('bedroom') === 'two' && bedroomDropdown.selectedIndex == 1) ) {
             card.classList.remove('show');
+        } else {
+            card.classList.add('show');
         }
     });
+});
+
+
+propertyCards.forEach(card => {
+  if (card.getAttribute('avail') === 'yes') {
+    card.classList.add('js_available'); // Add 'available' class to the card
+    card.querySelector('.available').textContent = 'Currently Available!';
+  } else {
+    // Optional: change the text to 'Currently Unavailable' if avail is not 'yes'
+    card.querySelector('.available').textContent = 'Currently Unavailable'; 
+  }
 });
