cd2423704129e391e9ab0ebf9c9db365ceafce03
KaiBatley <kaisbat@gmail.com>
2024-03-14 04:27:18 -0500

    separated js files

--- documentDropdown.js
+++ documentDropdown.js
@@ -5,15 +5,3 @@ const dropdown = document.getElementById('redirect-dropdown');
         window.location.href = selectedURL; 
       }
 });
-const form = document.getElementById('frm');
-
-form.addEventListener('submit', function(e) {
-
-    const hCaptcha = form.querySelector('textarea[name=h-captcha-response]').value;
-
-    if (!hCaptcha) {
-        e.preventDefault();
-        alert("Please fill out captcha field")
-        return
-    }
-});
